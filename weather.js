1aad1cce701da2154d9ea4320a42a12172f7b489
maximumayo <maxgmayo@gmail.com>
2017-02-25 20:36:18 -0500

    api call working

--- weather.js
+++ weather.js
@@ -1,3 +1,19 @@
-$(document).ready(function(){
-
+$(document).ready(function () {
+    $('#submit').click(function () {
+        var city = $('#city').val();
+        var key = "APPID=f21989bd78835a17e7bb52aae32725ab";
+        if (city != '') {
+            $.ajax({
+                url: "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial&" + key,
+                type: "GET",
+                dataTyple: "jsonp",
+                success: function (data) {
+                    console.log(data);
+                }
+            });
+        }
+        else {
+            alert("please enter location");
+        }
+    });
 });
